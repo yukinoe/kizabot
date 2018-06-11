2617118921d03b31bfcbe740a2e7f36e4dcad8c4
Yukinoe <Shafiq.82@gmail.com>
2018-06-11 13:43:02 +0200

    back to the test web app listening on port 8080

--- main.js
+++ main.js
@@ -125,8 +125,8 @@ app.get('/', function (req, res) {
 	console.log(req);	
 });
 
-app.listen(3000, function () {
-  console.log('App listening on port 3000!');
+app.listen(8080, function () {
+  console.log('App listening on port 8080!');
 });
 
 
